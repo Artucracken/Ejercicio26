c5944cad5dd019d09c4d0b773036a3bcd77889da
Arturo Herrera Gismero <arturo_h_gismero@hotmial.es>
2022-05-29 13:30:31 +0200

    añdadida interface Estudiante

---
+++ Ejercicio26/Estudiante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio26
+{
+    internal interface Estudiante 
+    {
+ 
+
+        public int NotaFinal(int nota)
+        {
+            if(nota < 5)
+            {
+                return 0;
+            }else
+            return 10;
+            
+        }
+
+        public String Recomendacion()
+        {
+           return "empresa milloneti";
+        }
+
+      
+
+
+
+    }
+}
